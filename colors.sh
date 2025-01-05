73d1253aa5def1dda7efaa75159c83fbf66e5c26
BLBMS <119702035+BLBMS@users.noreply.github.com>
2025-01-05 21:51:37 +0100

    Create colors.sh

---
+++ colors.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# v.2025-01-05
+# by blbMS
+Gray='\x1B[0;30m'
+Red='\x1B[0;31m'
+Green='\x1B[0;32m'
+Yellow='\x1B[0;33m'
+Blue='\x1B[0;34m'
+Magenta='\x1B[0;35m'
+Cyan='\x1B[0;36m'
+White='\x1B[0;37m'
+iGray='\x1B[1;30m'
+iRed='\x1B[1;31m'
+iGreen='\x1B[1;32m'
+iYellow='\x1B[1;33m'
+iBlue='\x1B[1;34m'
+iMagenta='\x1B[1;35m'
+iCyan='\x1B[1;36m'
+iWhite='\x1B[1;37m'
+xBlue='\x1B[1;94m'
+C_Off='\x1B[0m'
